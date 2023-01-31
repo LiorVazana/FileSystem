7b259ef099496071f4402b41ed7f6e6b9758e97f
Lior Vazana <liorvazana12@gmail.com>
2023-02-01 01:36:39 +0200

    Added API for Inodes and blocks management

--- FileSystem/FileSystem.cpp
+++ FileSystem/FileSystem.cpp
@@ -9,4 +9,60 @@ FileSystem::FileSystem(const std::string& fileName)
 	{
 		m_blockDevice.Write(reinterpret_cast<byte*>(&defaultInode), i * sizeof(Inode), sizeof(Inode));
 	}
+
+	byte isBlockFree = 0;
+
+	for (int i = 0; i < NUM_OF_BLOCKS; ++i)
+	{
+		m_blockDevice.Write(&isBlockFree, NUM_OF_INODES * sizeof(Inode) + i, 1);
+	}
+}
+
+Inode FileSystem::GetInodeFromOffset(const offset_t offset)
+{
+	Inode inode;
+	m_blockDevice.Read(reinterpret_cast<byte*>(&inode), offset, sizeof(Inode));
+	return inode;
+}
+
+offset_t FileSystem::GetFreeInodeOffset()
+{
+	for (int i = 0; i < NUM_OF_INODES; ++i)
+	{
+		if (GetInodeFromOffset(i * sizeof(Inode)).InodeType == InodeType::FREE)
+			return i * sizeof(Inode);
+	}
+	throw InvalidInput("There are no free Inodes left. :(");
+}
+
+void FileSystem::SetInodeFromOffset(const offset_t offset, const Inode& inode)
+{
+	m_blockDevice.Write(reinterpret_cast<const byte*>(&inode), offset, sizeof(Inode));
+}
+
+offset_t FileSystem::GetFreeBlockOffset()
+{
+	byte isBlockFree;
+
+	for (int i = 0; i < NUM_OF_BLOCKS; ++i)
+	{
+		m_blockDevice.Read(&isBlockFree, BLOCK_BITMAP_OFFEST + i, 1);
+
+		if (isBlockFree)
+			return BLOCKS_START_OFFEST + (i * SIZE_OF_BLOCK);
+	}
+}
+
+void FileSystem::WriteIntoBlock(const byte* const data, const offset_t offset, const size_t count)
+{
+	if (count > SIZE_OF_BLOCK)
+		throw InvalidInput("Can't write more than 256 bytes to block.");
+
+	m_blockDevice.Write(data, offset, count);
+}
+
+void FileSystem::SetBlockStateByOffset(const bool state, const offset_t offset)
+{
+	offset_t blockInBitmapOffset = BLOCK_BITMAP_OFFEST + ((offset - BLOCKS_START_OFFEST) / SIZE_OF_BLOCK);
+	m_blockDevice.Write(reinterpret_cast<const byte*>(&state), offset, 1);
 }
--- FileSystem/FileSystem.h
+++ FileSystem/FileSystem.h
@@ -3,11 +3,22 @@
 #include "BlockDevice.h"
 #include "Inode.h"
 
+typedef size_t offset_t;
+
 class FileSystem
 {
 public:
 	// C'tor
 	FileSystem(const std::string& fileName);
+
+	Inode GetInodeFromOffset(const offset_t offset);
+	offset_t GetFreeInodeOffset();
+	void SetInodeFromOffset(const offset_t offset, const Inode& inode);
+
+	offset_t GetFreeBlockOffset();
+	void WriteIntoBlock(const byte* const data, const offset_t offset, const size_t count);
+
+	void SetBlockStateByOffset(const bool state, const offset_t offset);
 
 private:
 	BlockDevice m_blockDevice;
@@ -19,4 +30,6 @@ private:
 	static constexpr size_t NUM_OF_INODES = 512;
 	static constexpr size_t SIZE_OF_BLOCK = 256;
 	static constexpr size_t NUM_OF_BLOCKS = (BLOCK_DEVICE_SIZE - (NUM_OF_INODES * sizeof(Inode))) / SIZE_OF_BLOCK;
+	static constexpr offset_t BLOCK_BITMAP_OFFEST = sizeof(Inode) * NUM_OF_INODES;
+	static constexpr offset_t BLOCKS_START_OFFEST = BLOCK_BITMAP_OFFEST + NUM_OF_BLOCKS;
 };
